5fc4331a6eba6f3f308f3703af15de4d89ef0a92
jeanne <jeanne.ngassa@gmail.com>
2015-11-27 09:52:48 +0100

    dernier commit

--- src/SortPeople.js
+++ src/SortPeople.js
@@ -1,4 +1,9 @@
+function compare(a,b){
+return a.age-b.age;}
 function sortPeople(people) {
+  if (!Array. isArray(people) || people.length===0){return null;}
+  /*else if (people.length===0) {return null;
+  }*/
+else {return people.sort(compare)}
 }
-
 module.exports = sortPeople;
